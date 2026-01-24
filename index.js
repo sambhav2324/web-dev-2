96f40b10b20ca157f078f61a5ea79d71ec738059
Sambhav Agarwal <sambhavagarwal49@gmail.com>
2026-01-24 18:49:38 +0530

    hi harry this side

---
+++ index.js
@@ -0,0 +1,53 @@
+// const handleSubmit=(event)=>{
+//     event.preventDefault();
+//     console.log("Form Submitted")
+// }
+
+// const form=document.getElementById("myForm");
+// form.addEventListener("submit",handleSubmit);
+   
+// const btn=document.querySelector("button")
+// const div=document.querySelector(".container")
+// const div2=document.querySelector(".outer")
+
+// btn.addEventListener("click",function(){ console.log("button")}, true)
+// div.addEventListener("click",function(){ console.log("Div")}, true)
+// div2.addEventListener("click",function(){ console.log("Div2")}, true)
+
+
+// debugger
+// console.log(a)
+// console.log(b)
+// var a =78
+// let b = 345
+// console.log(a)
+// console.log(b)
+
+// function print(){
+//     let c= 45
+//     console.log(c)
+//     console.log("inside fn")
+// }
+// print()
+
+// debugger
+// function first(){
+//     second()
+// }
+// function second(){
+//     third()
+// }
+// function third(){
+//     first()
+//     console.trace()
+// }
+// first()
+
+let total = 100
+
+function calculate(){
+    console.log(total)
+    let total=100
+}
+calculate()
+
