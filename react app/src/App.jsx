4ff13b116f4e8eeecb1658421cb767d9b63b6664
Sambhav Agarwal <sambhavagarwal49@gmail.com>
2026-04-13 13:34:01 +0530

    css module

--- react app/src/App.jsx
+++ react app/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { add, subtract } from './utility'
-import News from './component/Sample'
+import Sample from './component/Sample'
 import HomePage from './component/HomePage'
 
 
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <>
-      <News count={count} setCount={setCount} />
+      <Sample count={count} setCount={setCount} />
       <HomePage />
     </>
   )
--- react app/src/Components/Sample.jsx
+++ react app/src/Components/Sample.jsx
@@ -1,29 +1,48 @@
-import React, { Component, useState } from 'react'
+import React,{Component, useState} from 'react'
+import style from '../css/Sample.module.css'
+import style1 from '../css/HomePage.module.css'
+// import styled from 'styled-components'
+import styled from '@emotion/styled'
 
 function Sample(props) {
-    const [name, setName] = useState("Devendra")
-    const [age, setAge] = useState(20)
+    const [name,setName]=useState("Devendra")
+    const [age,setAge]=useState(20)
 
+    
 
-
-    return (
-        <div>
-            <h2>Trending topics</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae maiores, nobis laboriosam non commodi quia consequuntur vitae dolore facere deserunt quas recusandae iste harum in, provident aut reprehenderit temporibus.</p>
-            <h3>{name}</h3>
-            <button onClick={() => {
-                setName("Alex")
-
-            }}>Name Change</button>
-            <button
-                className="counter"
-                onClick={() => props.setCount((count) => count + 1)}
-            >
-                Count is {props.count}
-            </button>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Trending topics</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae maiores, nobis laboriosam non commodi quia consequuntur vitae dolore facere deserunt quas recusandae iste harum in, provident aut reprehenderit temporibus.</p>
+      <h3>{name}</h3>
+        <button onClick={()=>{
+            setName("Alex")
+            
+        }}>Name Change</button>
+    <button
+        className="counter"
+        onClick={() => props.setCount((count) => count + 1)}
+    >
+        Count is {props.count}
+    </button>
+    <div>
+        <Button >First</Button>
+        <Button green>Second</Button>
+    </div>
+    </div>
+  )
 }
+
+export default Sample
+
+const Button=styled.button`
+    background-color: ${(props) => (props.green ? "green" : "red")};
+    color: white;
+    width: 100px;
+    height: 35px;
+    border-radius: 10px;
+    border:none
+`
 
 // class Sample extends Component {
 //     constructor(props){
@@ -33,6 +52,26 @@ function Sample(props) {
 //             age:23
 //         }
 //     }
+//     componentDidMount(){
+//         console.log("Component Did Mount")
+//     }
+
+//     shouldComponentUpdate(nextProps,nextState){  //tels whether to update or not
+//         if(nextProps.count===1){
+//             return false
+//         }
+//         console.log("Component Should Update")
+//         return true
+//     }
+
+//     componentDidUpdate(){
+//         console.log("Component Did Update")
+//     }
+
+//     componentWillUnmount(){
+//         console.log("Component Will Unmount")
+//     }
+
 
 //     render(){
 //         return (
@@ -41,18 +80,19 @@ function Sample(props) {
 //             <h3>{this.state.name}</h3>
 //             <button onClick={()=>{
 //                 this.setState({name:"Alex"})
-
+                
 //                 }}>Name Change</button>
 //             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae maiores, nobis laboriosam non commodi quia consequuntur vitae dolore facere deserunt quas recusandae iste harum in, provident aut reprehenderit temporibus.</p>
 //             <button
-//                 className="counter"
+//                 style={{backgroundColor:"red"}}
 //                 onClick={() => this.props.setCount((count) => count + 1)}
 //             >
 //                 Count is {this.props.count}
 //             </button>
+            
 //             </div>
 //         )
 //     }
 
 // }
-export default Sample
+// export default Sample
